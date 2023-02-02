1c1e8a83f0518a31ba7380c354670cba1b91cfce
Dobrobatko Dmytro <dobrobatka@gmail.com>
2023-02-03 01:59:51 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,7 +7,7 @@ btnStop.addEventListener('click', onBtnStopClick);
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-let inrervalId = null;
+let intervalId = null;
 
 function onBtnStartClick() {
   btnStart.disabled = true;

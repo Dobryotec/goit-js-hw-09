b48b6fd7f4e579ec1ba0b39c520d8b63bcff929a
Dobryotec <112084445+Dobryotec@users.noreply.github.com>
2023-02-02 16:23:34 +0000

    Deploying to gh-pages from @ Dobryotec/goit-js-hw-09@3e47878c7841f4f0e4e232d7e2ef285dc32101d6 🚀

---
+++ 01-color-switcher.325792da.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,intervalId=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(intervalId)}))}();
+//# sourceMappingURL=01-color-switcher.325792da.js.map
---
+++ 01-color-switcher.a9803806.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,intervalId=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.a9803806.js.map
